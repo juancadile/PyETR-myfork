5257db353b3ab3de59a61beea43d004359d14b64
mark-todd <markpeter.todd@hotmail.co.uk>
2023-07-14 13:10:13 +0100

    Added stateset methods, matryoshka check, emphasis check

--- examples/new.py
+++ examples/new.py
@@ -1,8 +1,6 @@
 
 from dataclasses import dataclass
-from typing import Generic, Optional, TypeVar
-
-
+from typing import AbstractSet, Iterable, Optional
 
 
 class Function:
@@ -30,17 +28,40 @@ class ArbitraryObject:
         self.is_existential = is_existential
 
 class Emphasis:
+    term: "Term | ArbitraryObject"
     def __init__(self, t: "Term | ArbitraryObject") -> None:
-        pass
-
+        self.term = t
 
 class Term:
-    def __init__(self, f: Function, t: Optional[tuple["Term | ArbitraryObject | Emphasis"]] = None):
-        if t is None and f is not None and f.arity > 0:
-            raise ValueError("Inconsistent")
+    f: Function
+    t: Optional[tuple["Term | ArbitraryObject | Emphasis", ...]]
+    has_emphasis: bool
+    def __init__(self, f: Function, t: Optional[tuple["Term | ArbitraryObject | Emphasis", ...]] = None):
+        if t is None and f.arity > 0:
+            raise ValueError("Term not supplied when Function arity is greater than 0")
+        elif t is not None and (len(t) != f.arity):
+            raise ValueError(f"Term length {len(t)} did not match arity {f.arity}")
         self.f = f
         self.t = t
+        if t is None:
+            self.has_emphasis = False
+        else:
+            emphasis_count = self._count_emphasis(t)
+            if emphasis_count > 1:
+                raise ValueError(f"Emphasis in term with func: {self.f} greater than 1. Count: {emphasis_count})")
+            else:
+                self.has_emphasis = emphasis_count == 1
 
+    @staticmethod
+    def _count_emphasis(t: tuple["Term | ArbitraryObject | Emphasis", ...]) -> int:
+        emphasis_count = 0
+        for element in t:
+            if isinstance(element, Emphasis):
+                emphasis_count += 1
+            elif isinstance(element, Term):
+                emphasis_count += element.has_emphasis
+
+        return emphasis_count
 
 # Changed if clause in 4.2 to separate Arbitrary Objects from Term
 
@@ -78,6 +99,9 @@ equals_p = Predicate("=", 2)
 
 
 class Atom:
+    predicate: Predicate
+    terms: tuple[Term | ArbitraryObject | Emphasis,...]
+    has_emphasis: bool
     def __init__(
         self, 
         predicate: Predicate, 
@@ -86,13 +110,45 @@ class Atom:
         if len(terms) != predicate.arity:
             raise ValueError("Inconsistent")
         self.predicate = predicate
+        emphasis_count = 0
+        for term in terms:
+            if isinstance(term, Term):
+                emphasis_count += term.has_emphasis
+        if emphasis_count > 1:
+            raise ValueError(f"Emphasis count in atom at: {emphasis_count}")
+        else:
+            self.has_emphasis = emphasis_count == 1
         self.terms = terms
-        #atom has one or 0 emphasis
 
-T = TypeVar("T", bound=set)
-class stateset(frozenset[T], Generic[T]):
-    def __init__(self, atoms: set[T] | frozenset[T]) -> None:
-        self.atoms = atoms
+        # Invarient: atom has one or 0 emphasis
+
+
+class stateset(frozenset[Atom]):
+    def __new__(cls, __iterable: Optional[Iterable[Atom]] = None, /) -> "stateset":
+        if __iterable is None:
+            return super().__new__(cls)
+        else:
+            return super().__new__(cls, __iterable)
+
+    def copy(self) -> "stateset":
+        return stateset(super().copy())
+    def difference(self, *s: Iterable[object]) -> "stateset":
+        return stateset(super().difference(*s))
+    def intersection(self, *s: Iterable[object]) -> "stateset":
+        return stateset(super().intersection(*s))
+    def symmetric_difference(self, __s: Iterable[Atom]) -> "stateset":
+        return stateset(super().symmetric_difference(__s))
+    def union(self, *s: Iterable[Atom]) -> "stateset":
+        return stateset(super().union(*s))
+    def __and__(self, __value: AbstractSet[Atom]) -> "stateset":
+        return stateset(super().__and__(__value))
+    def __or__(self, __value: AbstractSet[Atom]) -> "stateset":
+        return stateset(super().__or__(__value))
+    def __sub__(self, __value: AbstractSet[Atom]) -> "stateset":
+        return stateset(super().__sub__(__value))
+    def __xor__(self, __value: AbstractSet[Atom]) -> "stateset":
+        return stateset(super().__xor__(__value))
+
 
 def get_arb_objects(states:stateset) -> tuple[set[ArbitraryObject], set[ArbitraryObject]]:
     raise NotImplementedError
@@ -106,18 +162,16 @@ class Dependency:
     existentials: set[ArbitraryObject]
 
 
-def compare_existentials(deps: set[Dependency]):
+def test_matroyshka(deps: set[Dependency]):
     existentials: list[set[ArbitraryObject]] = [
         d.existentials for d in deps
     ]
-    #compare any two sets of existentials, with is_related
-    raise NotImplementedError
-    # stack = copy(existentials)
-    # while stack:
-    #     ext1 = stack.pop(0)
-    # for e1 in existentials:
-
-
+    stack = existentials.copy()
+    while stack:
+        compare_set1 = stack.pop(0)
+        for compare_set2 in stack:
+            if not is_related(compare_set1, compare_set2):
+                raise ValueError(f'Existential sets do not meet Matroyshka condition. \nSet1: {compare_set1}\nSet2: {compare_set2}')
 
 
 
@@ -128,8 +182,7 @@ class DependencyRelation:
         self, 
         dependencies: set[Dependency]
     ) -> None:
-        compare_existentials(dependencies)
-
+        test_matroyshka(dependencies)
         self.dependencies = dependencies
     def validate(self, states: stateset):
         uni_arb_objects, exi_arb_objects = get_arb_objects(states)
@@ -153,7 +206,7 @@ class View:
     ) -> None:
         self.stage = stage
         self.supposition = supposition
-        dependency_relation.validate(stateset(stage.union(supposition)))
+        dependency_relation.validate(stage.union(supposition))
         self.dependency_relation = dependency_relation
 
         #view has exactly one emphasis
@@ -163,8 +216,3 @@ existential_arb_set = ArbitraryObjectMaker(is_existential=True)
 john_smokes = Atom(smokes, (Term(f=Function("john", 0)),))
 existent_arb_obj = next(existential_arb_set)
 arbitrary_object_smokes = Atom(smokes, (Emphasis(existent_arb_obj),))
-
-
-
-
-
