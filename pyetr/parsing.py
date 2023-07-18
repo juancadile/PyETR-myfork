7d9602e26b8b4e45c505c416b74f2dbde68fc3e6
mark-todd <markpeter.todd@hotmail.co.uk>
2023-07-18 19:31:23 +0100

    Added first fully operational parser

--- pyetr/parsing.py
+++ pyetr/parsing.py
@@ -1,7 +1,20 @@
 import pyparsing as pp
+from pyparsing import ParserElement
+
+ParserElement.enablePackrat()
 
 pp_left = pp.opAssoc.LEFT  # type:ignore
 pp_right = pp.opAssoc.RIGHT  # type:ignore
+
+
+class Atom:
+    var: str
+
+    def __init__(self, t) -> None:
+        self.var = t[0]
+
+    def __repr__(self) -> str:
+        return f"<Atom var={self.var}>"
 
 
 class Pred:
@@ -54,52 +67,68 @@ class BoolOr:
         return f"<BoolOr operands={self.operands}>"
 
 
+class Implies:
+    def __init__(self, t) -> None:
+        self.operands = t[0]
+
+    def __repr__(self) -> str:
+        return f"<Implies operands={self.operands}>"
+
+
+class Equals:
+    def __init__(self, t) -> None:
+        self.operands = t[0]
+
+    def __repr__(self) -> str:
+        return f"<Equals operands={self.operands}>"
+
+
 expr = pp.Forward()
-atom = pp.Word(pp.alphas, pp.alphanums).setResultsName("atom")
+atom = pp.Word(pp.alphas, pp.alphanums).setResultsName("atom").setParseAction(Atom)
 
 quantifier = pp.oneOf(
     "∃ ∀",
 ).setResultsName("quantifier")
 quantified_expr = pp.Group(quantifier + atom).setParseAction(Quantified)
 
-bool_not = pp.Suppress(pp.Char("~"))
-bool_or = pp.Suppress(pp.Char("∨"))
-bool_and = pp.Suppress(pp.Char("∧"))
+bool_not = pp.Suppress(pp.oneOf("~"))
+bool_or = pp.Suppress(pp.oneOf("∨ |"))
+bool_and = pp.Suppress(pp.oneOf("∧ &"))
+implies = pp.Suppress(pp.Char("→"))
+equals = pp.Suppress(pp.oneOf("="))
 
-predicate = (
-    pp.Group(
-        pp.Word(pp.alphas, pp.alphanums).setResultsName("predicate")
-        + pp.Suppress("(")
-        + atom
-        + pp.Suppress(")")
-    )
-    .setResultsName("predicate_grp", listAllMatches=True)
-    .setParseAction(Pred)
-)
+predicate = pp.Group(
+    pp.Word(pp.alphas, pp.alphanums).setResultsName("predicate")
+    + pp.Suppress("(")
+    + atom
+    + pp.Suppress(")")
+).setParseAction(Pred)
 
 nested_and = pp.infixNotation(
-    predicate,
+    predicate | atom,
     [
         (bool_not, 1, pp_right, BoolNot),
         (bool_and, 2, pp_left, BoolAnd),
         (bool_or, 2, pp_left, BoolOr),
+        (equals, 2, pp_left, Equals),
+        (implies, 2, pp_left, Implies),
     ],
     lpar=pp.Suppress("("),
     rpar=pp.Suppress(")"),
-).setResultsName("logical", listAllMatches=True)
+)
 expr <<= pp.ZeroOrMore(quantified_expr) + nested_and
 
-
-def parse_formula(input_string):
-    return expr.parse_string(input_string, parseAll=True)
+Item = Atom | Pred | Quantified | BoolNot | BoolAnd | BoolOr | Implies | Equals
 
 
-# input_string = '∃x ∃y ∀z ∀w [King(x) ∧ Queen(y) ∧ [King(z) → z = x] ∧ [Queen(w) → w = y]]'
+def parse_formula(input_string: str) -> list[Item]:
+    return expr.parse_string(input_string, parseAll=True).as_list()
+
+
+input_string = (
+    "∃x ∃y ∀z ∀w (King(x) ∧ Queen(y) ∧ (King(z) → z = x) ∧ (Queen(w) → w = y))"
+)
 # input_string = "∃x ∃y ∀z ∀w [King(x) ∧ Queen(y) ∧ [King(x) ∧ Jack(y)]]"
-input_string = "∃x ∃y ∀z ∀w (King(x) ∨ ~Queen(y) ∧ (King(x) ∧ Jack(y)))"
+# input_string = "∃x ∃y ∀z ∀w (King(x) ∨ ~Queen(y) ∧ (King(x) ∧ Jack(y)))"
 result = parse_formula(input_string)
-
-print(result.as_list())
-
-# bits to add
-# implication = pp.Group(expr + pp.Suppress('→') + expr)
+print(result)
