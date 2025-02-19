08817b1d970b78c7d88b2c65e53da1a8fa013a8f
mark-todd <markpeter.todd@hotmail.co.uk>
2025-02-19 15:08:05 +0000

    Added first version of the unparser

--- pyetr/parsing/smt_parser/__init__.py
+++ pyetr/parsing/smt_parser/__init__.py
@@ -1,1 +1,2 @@
 from .parse_smt import smt_to_view
+from .unparse_smt import view_to_smt
---
+++ pyetr/parsing/smt_parser/unparse_smt.py
@@ -0,0 +1,102 @@
+import typing
+
+from pysmt.environment import Environment
+from pysmt.fnode import FNode
+from pysmt.formula import FormulaManager
+from pysmt.typing import BOOL, INT, REAL, FunctionType
+
+from pyetr.parsing.common import Quantified, Variable
+from pyetr.parsing.fol_items import (
+    BoolAnd,
+    BoolOr,
+    Implies,
+    Item,
+    LogicPredicate,
+    view_to_items,
+)
+from pyetr.parsing.fol_items.items import Falsum, LogicReal, Truth
+
+if typing.TYPE_CHECKING:
+    from pyetr.view import View
+
+
+def items_to_smt(items: list[Item], formula_manager: FormulaManager):
+    def item_to_smt(item: Item) -> FNode:
+        if isinstance(item, Implies):
+            left = item_to_smt(item.left)
+            right = item_to_smt(item.right)
+            return formula_manager.Implies(left=left, right=right)
+        elif isinstance(item, LogicReal):
+            return formula_manager.Real(item.num)
+        elif isinstance(item, LogicPredicate):
+            name = item.name
+            arg_types = []
+            for arg in item.args:
+                if isinstance(arg, LogicReal):
+                    arg_types.append(REAL)
+                else:
+                    arg_types.append(BOOL)
+            if name == "==":
+                assert len(item.args) == 2
+                return formula_manager.EqualsOrIff(
+                    item_to_smt(item.args[0]), item_to_smt(item.args[1])
+                )
+            else:
+                vname = formula_manager.Symbol(
+                    name=name,
+                    typename=FunctionType(return_type=BOOL, param_types=arg_types),
+                )
+                return formula_manager.Function(
+                    vname=vname, params=[item_to_smt(i) for i in item.args]
+                )
+        elif isinstance(item, Variable):
+            return formula_manager.Symbol(item.name)
+        elif isinstance(item, BoolAnd):
+            return formula_manager.And(*[item_to_smt(i) for i in item.operands])
+        elif isinstance(item, BoolOr):
+            return formula_manager.Or(*[item_to_smt(i) for i in item.operands])
+        elif isinstance(item, Truth):
+            return formula_manager.TRUE()
+        elif isinstance(item, Falsum):
+            return formula_manager.FALSE()
+        else:
+            raise NotImplementedError(f"{item}, {item.__class__}")
+
+    # First separate quantifieds
+    view_item = None
+    quantifieds: list[Quantified] = []
+    for item in items:
+        if isinstance(item, Quantified):
+            quantifieds.append(item)
+        else:
+            assert view_item is None  # There must only be one valid view
+            view_item = item
+    if view_item is None:
+        raise ValueError(f"Main section not found")
+
+    # parse main item
+    out = item_to_smt(view_item)
+    current_quants = []
+    last_quantifier = None
+    for quant in reversed(quantifieds):
+        if last_quantifier is not None and quant.quantifier != last_quantifier:
+            if last_quantifier == "∀":
+                out = formula_manager.ForAll(current_quants, out)
+            else:
+                out = formula_manager.Exists(current_quants, out)
+            current_quants = []
+        current_quants.insert(0, item_to_smt(quant.variable))
+        last_quantifier = quant.quantifier
+    if current_quants:
+        if last_quantifier == "∀":
+            out = formula_manager.ForAll(current_quants, out)
+        else:
+            out = formula_manager.Exists(current_quants, out)
+    return out
+
+
+def view_to_smt(v: "View", env: typing.Optional[Environment] = None) -> FNode:
+    if env is None:
+        env = Environment()
+    formula_manager = env.formula_manager
+    return items_to_smt(view_to_items(v), formula_manager)
